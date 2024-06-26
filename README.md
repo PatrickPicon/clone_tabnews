6a32c1697e94b917d79682cb0d99ab6a5ce2b20d
Patrick <79381931+PatrickPicon@users.noreply.github.com>
2024-06-26 10:35:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone_tabnews
+
 Um novo projeto de desenvolvimento completo
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
-    return <h1>Thânia vai um desafio, se acertar ganha um presente e um vale pedido. É algo importante e usado no dia a dia, as vezes muitas pessoas exageram
-    que dá até para sentir quando passam do outro lado da rua, ou quando acertam e você quer até chegar mais perto dependendo da pessoa,
-    mas se ficam sem, as vezes é dificil até ficar perto, o que é?</h1>
+  return (
+    <h1>
+      Thânia vai um desafio, se acertar ganha um presente e um vale pedido. É
+      algo importante e usado no dia a dia, as vezes muitas pessoas exageram que
+      dá até para sentir quando passam do outro lado da rua, ou quando acertam e
+      você quer até chegar mais perto dependendo da pessoa, mas se ficam sem, as
+      vezes é dificil até ficar perto, o que é?
+    </h1>
+  );
 }
 
 export default Home;
